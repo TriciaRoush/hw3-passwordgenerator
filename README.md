c1709bb41ee28a975f8f9c87199a073260cd59c3
Tricia R <triciaroush@live.com>
2020-09-16 21:05:09 -0400

    Finalization of JavaScript code and completion of README file.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hw3-passwordgenerator
+This project is a designated assignment for the Univesity of Penn LPS Coding Bootcamp.  The objective is to create an application that will generate a random password based on user-selected criteria.  
+The application will run in the browser and feature dynamically updated HTML and CSS powered by JavaScript code. It also features a clean and polished user interface and is responsive, ensuring that it adapts to multiple screen sizes.
+
+-Upon initialization of the app by clicking the "Generate Password" button, the user will be prompted for the length of the intended password.  The code ensures the password can only contain at minimum 8 characters and at maximum, 128 character. If the user selects an answer outside of this range, they will receive an alert about the alloted size and will be promted to start again.  
+-The user will now be directed through four seperate confirms.  The first for a capital letter, the second for a lowercase letter, the third for a numerical character and the final for a symbol. Each character is mandatory for the completion of the password and if any are not confirmed, the user is alerted that each is mandatory and to begin the character selection again.
+-Once all four confirms' conditions have been met, a password output will generate in the designated text box.  
--- script.js
+++ script.js
@@ -12,3 +12,63 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Declaring variables
+var upperCaseCharacter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'];
+var numberCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var symbolCharacter = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|',';', ':', '<', '>', ',', '.', '/', '?'];
+var confirmLength = "";
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumberCharacter;
+var confirmSymbolCharacter;
+var passwordResult = "";
+
+//Defining the function
+function generatePassword() {
+    //Length promt with loop
+    var confirmLength = prompt("How many characters does your password require?");
+while (confirmLength <= 7 || confirmLength >= 129) {
+    alert("Password length must be between 8 - 128 chracters.  Please try again.");
+    var confirmLength = prompt("How many characters does your password require?");
+}
+//Character prompts with loop
+var confirmUpperCase = confirm("Click OK if you wish to include an UPPER CASE LETTER in your password.");
+var confirmLowerCase = confirm("Click OK if you wish to include a LOWER CASE LETTER in your password.");
+var confirmNumberCharacter = confirm("Click OK if you wish to include a NUMERIC CHARACTER in your password.");
+var confirmSymbolCharacter = confirm("Click OK if you wish to include a SYMBOL CHARACTER in your password.");
+while (confirmUpperCase === false ||
+    confirmLowerCase === false ||
+    confirmNumberCharacter === false ||
+    confirmSymbolCharacter === false) {
+        alert("These four characters are requirements of a secure password. Please begin again.");
+        var confirmUpperCase = confirm("Click OK if you wish to include an UPPER CASE LETTER in your password.");
+        var confirmLowerCase = confirm("Click OK if you wish to include a LOWER CASE LETTER in your password.");
+        var confirmNumberCharacter = confirm("Click OK if you wish to include a NUMERIC CHARACTER in your password.");
+        var confirmSymbolCharacter = confirm("Click OK if you wish to include a SYMBOL CHARACTER in your password.");
+    }
+    //Combines confirmed results
+if (confirmUpperCase) {
+    passwordResult = passwordResult.concat(upperCaseCharacter);
+}
+if (confirmLowerCase) {
+    passwordResult = passwordResult.concat(lowerCaseCharacter);
+}
+if (confirmNumberCharacter) {
+    passwordResult = passwordResult.concat(numberCharacter);
+}
+if (confirmSymbolCharacter) {
+    passwordResult = passwordResult.concat(symbolCharacter);
+}
+console.log(passwordResult);
+//Renders password 
+var randomPassword = "";
+for (var i=0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordResult[Math.floor(Math.random() * passwordResult.length)];
+    console.log(randomPassword);
+}
+return randomPassword;
+}
+
+
